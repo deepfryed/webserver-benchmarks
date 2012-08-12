b441679709046cf2f4c61337acf0b00dfb50d8a4
Bharanee Rathna <deepfryed@gmail.com>
2012-08-12 14:32:27 +1000

    memcheck & tidy up

--- bechmark.sh
+++ bechmark.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
+iter=10000
+
+function memcheck() {
+  ppid=$1
+  echo
+  echo "##########################################################################################"
+  echo "memory use (rss)"
+  echo "##########################################################################################"
+  echo
+  echo "parent:"
+  ps -o rss= --pid  $ppid
+  echo "workers:"
+  ps -o rss= --ppid $ppid
+  echo
+}
+
 echo "##########################################################################################"
 echo "testing puma"
 echo "##########################################################################################"
-echo
 
-./bin/puma -w 2 config.ru -q > /dev/null &
+./bin/puma -w 2 -q config.ru > /dev/null &
 pid=$!
 
+sleep 2
+ab -n$iter -c10 -q -k http://127.0.0.1:9292/ | head -n24 | tail -n11
+
 sleep 1
-ab -q -n2000 -c10 $@ http://127.0.0.1:9292/ | head -n24 | tail -n11
-kill -QUIT $pid
+memcheck $pid
 kill -TERM $pid
 
 sleep 1
 echo "##########################################################################################"
 echo "testing thin (with pastry)"
 echo "##########################################################################################"
-echo
 
 ./bin/pastry -p 9292 -R config.ru start > /dev/null &
 pid=$!
-sleep 1
 
-ab -q -n10000 -c10 $@ http://127.0.0.1:9292/ | head -n24 | tail -n11
-kill -QUIT $pid
+sleep 2
+ab -n$iter -c10 -q -k http://127.0.0.1:9292/ | head -n24 | tail -n11
+
+sleep 1
+memcheck $pid
+kill -TERM $pid
